ba761451cdadd44a1e3114d2d8fffb299e8f8b77
kulya91 <1942186814@qq.com>
2019-06-10 23:01:40 +0800

    v4.0不使用中断发送数据但是没试过，应该没问题

--- Usart.h
+++ Usart.h
@@ -4,27 +4,13 @@ unsigned char ttldat[5];
 	SCON=0X50;			//设置为工作方式1
 	TMOD=0X21;			//设置计数器工作方式2
 	PCON=0X80;			//波特率加倍
-
 	TH1=0XF3;				//计数器初始值设置，注意波特率是4800的
 	TL1=0XF3;
-
-	TH0=0XFd;	//给定时器赋初值，定时1ms
-	TL0=0Xfd;
 	ET0=1;
 	TR0=1;
 	ES=1;						//打开接收中断
 	EA=1;						//打开总中断
 	TR1=1;					//打开计数器
-}
-void Timer0Init()
-{
-	TMOD|=0X01;//选择为定时器0模式，工作方式1，仅用TR0打开启动。
-
-	TH0=0XFC;	//给定时器赋初值，定时1ms
-	TL0=0X18;	
-	ET0=1;//打开定时器0中断允许
-	EA=1;//打开总中断
-	TR0=1;//打开定时器			
 }
 
   void SendChar(unsigned char dat)
@@ -47,14 +33,15 @@ void Timer0Init()
 void SendData()
 {	
     unsigned int i;
-    unsigned char a[]="温度:";		   
-	 //  SendString(a);
+    //unsigned char a[]="温度:";		   
+	//SendString(a);
 	 SendChar('T');
 	   for(i=0;i<5;i++)
 	   {   if(i==3)
 	   		 SendChar('.');
 	     SendChar(ttldat[i]+0x30);
-    	}  //SendChar('C');
+    	}
+	  //SendChar('C');
 	    SendChar('\r');
 	    SendChar('\n');
 	
--- main.c
+++ main.c
@@ -4,51 +4,31 @@
 #include"Usart.h"
  #include"1602.h"
 
-typedef unsigned int u16;	  //对数据类型进行声明定义
-typedef unsigned char u8;
-u8 bai,shi,ge,fen,li;
-u8 DisplayData[8];
-void delay(u16 i)
-{
-	while(i--);	
-}
-
-
+unsigned int bai,shi,ge,fen,li;
 void datapros(int temp) 	 
 {
    	float tp;  
-	if(temp< 0)				//当温度值为负数
+	if(temp< 0)	
   	{
-		DisplayData[0] = 0x40; 	  //   -
-		//因为读取的温度是实际温度的补码，所以减1，再取反求出原码
 		temp=temp-1;
 		temp=~temp;
 		tp=temp;
 		temp=tp*0.0625*100+0.5;	
-		//留两个小数点就*100，+0.5是四舍五入，因为C语言浮点数转换为整型的时候把小数点
-		//后面的数自动去掉，不管是否大于0.5，而+0.5之后大于0.5的就是进1了，小于0.5的就
-		//算加上0.5，还是在小数点后面。
- 
   	}
  	else
   	{			
-		DisplayData[0] = 0x00;
-		tp=temp;//因为数据处理有小数点所以将温度赋给一个浮点型变量
-		//如果温度是正的那么，那么正数的原码就是补码它本身
+		tp=temp;
 		temp=tp*0.0625*100+0.5;	
-		//留两个小数点就*100，+0.5是四舍五入，因为C语言浮点数转换为整型的时候把小数点
-		//后面的数自动去掉，不管是否大于0.5，而+0.5之后大于0.5的就是进1了，小于0.5的就
-		//算加上0.5，还是在小数点后面。
 	}
 	bai=temp / 10000;
-shi=temp % 10000 / 1000;
+    shi=temp % 10000 / 1000;
 	ge=temp % 1000 / 100;
 	fen=temp % 100 / 10;
 	li=temp % 10;
-	 if((li>=0&&li<=9)&&(fen>=0&&fen<=9)&&(ge>=0&&ge<=9)&(shi>=1&&shi<=5)&(bai==0))
+	 if((li>=0&&li<=9)&&(fen>=0&&fen<=9)&&(ge>=0&&ge<=9)&(shi>=1&&shi<=3)&(bai==0))
 	 {
 	ttldat[0]=bai; ttldat[1]=shi;ttldat[2]=ge;ttldat[3]=fen;ttldat[4]=li;
-		}
+	}
 }
 
  void xianshi()
@@ -60,29 +40,21 @@ shi=temp % 10000 / 1000;
 	lcddat('.');
 	lcddat(ttldat[3]+0x30);
 	lcddat(ttldat[4]+0x30);
+	lcddat('C');
 	
 }
 
 void main()
 {
 	lcdinit("temperature:");
-UsartInit();
+    UsartInit();
 
 	while(1)
 	{
-		datapros(Ds18b20ReadTemp()); //数据处理函数
+	datapros(Ds18b20ReadTemp()); 
+	xianshi();
+	SendData();
+	delayms(100);
 	}		
 }
-
-  void Usart() interrupt 1
-{  u16 i;
-	TH0=0XFC;	//给定时器赋初值，定时1ms
-	TL0=0X18;
-	i++;
-	if(i==100)
-	{
-		i=0;
-		SendData();
-		xianshi();		
-	}
-}	
+	
